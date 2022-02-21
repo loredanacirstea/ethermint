dfc6459c21c0113370ad47d79adece27b916585d
Loredana <loredana.cirstea@gmail.com>
2022-02-22 01:48:10 +0200

    dd

--- x/evm/vm/precompiles.go
+++ x/evm/vm/precompiles.go
@@ -1,7 +1,9 @@
 package extendedVM
 
 import (
+	"errors"
 	"fmt"
+	"math/big"
 	"time"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -37,6 +39,43 @@ func (c *ibcPrecompile) RequiredGas(input []byte) uint64 {
 }
 
 func (c *ibcPrecompile) Run(evm *vm.EVM, caller vm.ContractRef, input []byte) ([]byte, error) {
+	signature := common.Bytes2Hex(input[0:4])
+	callInput := input[4:]
+	var result []byte
+	var err error
+
+	fmt.Println("---ibcPrecompile-Run--", signature, callInput)
+
+	switch signature {
+	case "1fe143a5": // sendMessage(string,string,address,bytes)
+		result, err = sendMessage(c, caller, callInput)
+	case "cf68263a": // getMessage(string,string,address,uint256)
+		result, err = getMessage(c, caller, callInput)
+	case "e161b98f": // countMessages(string,string,address)
+		result, err = countMessages(c, caller, callInput)
+	default:
+		return nil, errors.New("invalid ibcPrecompile function")
+	}
+	if err != nil {
+		return nil, err
+	}
+	encodedResult := append(
+		new(big.Int).SetUint64(32).FillBytes(make([]byte, 32)),
+		new(big.Int).SetInt64(int64(len(result))).FillBytes(make([]byte, 32))...,
+	)
+	encodedResult = append(encodedResult, result...)
+
+	padding := 32 - len(encodedResult)%32
+	encodedResult = append(encodedResult, make([]byte, padding)...)
+
+	fmt.Println("--ibcPrecompile result--", encodedResult)
+	return encodedResult, err
+}
+
+// function sendMessage(string memory portId, string memory channelId, address target, bytes memory data) external returns(bool success);
+
+func sendMessage(c *ibcPrecompile, caller vm.ContractRef, input []byte) ([]byte, error) {
+
 	// channelCap := endpoint.Chain.GetChannelCapability(packet.GetSourcePort(), packet.GetSourceChannel())
 
 	portId := "controibc"
@@ -56,11 +95,10 @@ func (c *ibcPrecompile) Run(evm *vm.EVM, caller vm.ContractRef, input []byte) ([
 
 	// timeoutHeight := clienttypes.NewHeight(currentHeight.RevisionNumber, currentHeight.RevisionHeight+50)
 	timeoutHeight := clienttypes.NewHeight(2, 100000)
-	sequence, found := c.vmIbcKeeper.ChannelKeeper.GetNextSequenceSend(c.ctx, portId, channelId)
-	if !found {
-		sequence = 1
-	}
-
+	// sequence, found := c.vmIbcKeeper.ChannelKeeper.GetNextSequenceSend(c.ctx, portId, channelId)
+	// if !found {
+	// 	sequence = 1
+	// }
 
 	// packet := channeltypes.NewPacket([]byte("hello precompile"), sequence, portId, channelId, portId, channelId, timeoutHeight, timeoutTimestamp)
 
@@ -76,6 +114,17 @@ func (c *ibcPrecompile) Run(evm *vm.EVM, caller vm.ContractRef, input []byte) ([
 	err :=
 		c.vmIbcKeeper.TransmitVmibcMessagePacket(c.ctx, packetData, portId, channelId, timeoutHeight, timeoutTimestamp)
 
+	return []byte("hello"), err
+}
 
-	return []byte("hello"), nil
+func getMessage(c *ibcPrecompile, caller vm.ContractRef, input []byte) ([]byte, error) {
+	msg, success := c.vmIbcKeeper.GetVmIbcMessage(c.ctx, caller.Address().Bytes())
+	if !success {
+		return nil, errors.New("message not found")
+	}
+	return []byte(msg.Body), nil
+}
+
+func countMessages(c *ibcPrecompile, caller vm.ContractRef, input []byte) ([]byte, error) {
+	return nil, nil
 }
